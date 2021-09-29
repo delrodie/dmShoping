c1fcc16da2007b1ec297c0261c357525cd1f97f4
Delrodie AMOIKON <delrodieamoikon@gmail.com>
2021-09-29 07:12:20 +0000

    Integration layout fronted

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,19 +2,33 @@
 
 namespace App\Controller;
 
+use App\Entity\Album;
+use App\Entity\Slide;
+use App\Repository\AlbumRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
+
+    private $albumRepository;
+
+    public function __construct(AlbumRepository $albumRepository)
+    {
+        $this->albumRepository = $albumRepository;
+    }
+
     /**
      * @Route("/", name="app_home")
      */
     public function index(): Response
     {
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'albums' => $this->albumRepository->findBy(['ecommerce'=>true], ['id'=>'DESC']),
+            'promos' => $this->albumRepository->findBy(['promotion'=> true, 'ecommerce' => true], ['id'=>'DESC']),
+            'slides' => $this->getDoctrine()->getRepository(Slide::class)->findBy(['statut'=>true], ['id'=>'DESC'])
         ]);
     }
 }
---
+++ src/Controller/MenuController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Genre;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/menu")
+ */
+class MenuController extends AbstractController
+{
+    /**
+     * @Route("/", name="menu_horizontal")
+     */
+    public function index(): Response
+    {
+        return $this->render('menu/index.html.twig', [
+            'genres' => $this->getDoctrine()->getRepository(Genre::class)->findBy([],['libelle'=>"ASC"]),
+        ]);
+    }
+
+    /**
+     * @Route("/mobile", name="menu_mobile")
+     */
+    public function mobile()
+    {
+        return $this->render('menu/mobile.html.twig',[
+            'genres' => $this->getDoctrine()->getRepository(Genre::class)->findBy([],['libelle'=>"ASC"]),
+        ]);
+    }
+}
